c734e782e5b38e33008b8236ca49653685bbfbea
Bennah <bennah955@gmail.com>
2026-03-03 22:57:32 +0300

    Uploaded w4 tuesday's assignment

---
+++ w04d02.js
@@ -0,0 +1,26 @@
+function mergeArrays(a, b){
+  const merged = [...a, ...b];
+  return merged;
+}
+
+console.log(mergeArrays(
+  a = [1, 2], 
+  b=[3, 4]));
+
+
+function createGreeting(name = "Guest", greeting = "Hello"){
+  return `${greeting}, ${name}!`;
+}
+
+console.log(createGreeting());
+console.log(createGreeting("Benard"));
+console.log(createGreeting("Benard", "Jambo"));
+
+
+function calculateTotal(price, tax = 0.16){
+  let totalPrice = price + (tax * price);
+  return totalPrice;
+}
+
+console.log(calculateTotal(100));
+console.log(calculateTotal(100, 0.2));
